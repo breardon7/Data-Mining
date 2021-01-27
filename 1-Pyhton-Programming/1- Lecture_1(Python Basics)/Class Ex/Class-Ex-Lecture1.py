2da2d3773a824b8a1632ad23360bc40d2432ff3c
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-01-26 19:29:23 -0500

    Update Class-Ex-Lecture1.py

--- 1-Pyhton-Programming/1- Lecture_1(Python Basics)/Class Ex/Class-Ex-Lecture1.py
+++ 1-Pyhton-Programming/1- Lecture_1(Python Basics)/Class Ex/Class-Ex-Lecture1.py
@@ -1,8 +1,15 @@
-# =================================================================
-# Class_Ex1: 
-# Write python program that converts seconds to 
+# Class_Ex1:
+# Write python program that converts seconds to
 # (x Hour, x min, x seconds)
 # ----------------------------------------------------------------
+print('Enter a given number of seconds to convert seconds to hours and minutes:')
+s = eval(input())
+hours = s/3600
+mins = s/60
+secs = s
+print('Hours = ' + str(hours))
+print('Minutes = ' + str(mins))
+print('Seconds = ' + str(secs))
 
 
 
@@ -10,39 +17,79 @@
 
 
 # =================================================================
-# Class_Ex2: 
+# Class_Ex2:
 # Write a python program to print all the different arrangements of the
 # letters A, B, and C. Each string printed is a permutation of ABC.
 # ----------------------------------------------------------------
-
+print('All permutations of ABC:')
+for i in 'ABC':
+    first_term = i
+    for j in 'ABC':
+        second_term = j
+        if i != j:
+            for k in 'ABC':
+                third_term = k
+                if j != k and i!=k:
+                    print(i+j+k)
 
 
 
 
 # =================================================================
-# Class_Ex3: 
+# Class_Ex3:
 # Write a python program to print all the different arrangements of the
 # letters A, B, C and D. Each string printed is a permutation of ABCD.
 # ----------------------------------------------------------------
 
-
+print('All permutations of ABCD:')
+for i in 'ABCD':
+    first_term = i
+    for j in 'ABCD':
+        second_term = j
+        if i != j:
+            for k in 'ABCD':
+                third_term = k
+                if j != k and i!=k:
+                    for n in 'ABCD':
+                        if j != k and i!=k and n!=k and j!=n and i!=n:
+                            print(i+j+k+n)
 
 
 
 # =================================================================
-# Class_Ex4: 
-# Suppose we wish to draw a triangular tree, and its height is provided 
+# Class_Ex4:
+# Suppose we wish to draw a triangular tree, and its height is provided
 # by the user.
 # ----------------------------------------------------------------
+print("Input an integer for the triangular tree's height: ")
+
+h = eval(input())
+star = '* '
+space = ' '
+for i in range(0,h):
+   print(space*((h-i))+star*i)
 
 
 
 
 
 # =================================================================
-# Class_Ex5: 
+# Class_Ex5:
 # Write python program to print prime numbers up to a specified values.
 # ----------------------------------------------------------------
+print('Input an integer to find all prime numbers up to and including that integer: ')
+x = eval(input())
+
+for i in range(1, x+1):
+    if i == 1:
+        print(i)
+    elif i > 1:
+        for j in range(2, i):
+            if i % j == 0:
+                break
+            else:
+                print(i)
+                break
 
 
 
