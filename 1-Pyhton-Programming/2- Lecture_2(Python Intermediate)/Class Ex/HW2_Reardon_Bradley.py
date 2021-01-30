e6027f5fbfb712349c1305ed5225b432f6b9ff38
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-01-30 17:24:31 -0500

    Update HW2_Reardon_Bradley.py

--- 1-Pyhton-Programming/2- Lecture_2(Python Intermediate)/Class Ex/HW2_Reardon_Bradley.py
+++ 1-Pyhton-Programming/2- Lecture_2(Python Intermediate)/Class Ex/HW2_Reardon_Bradley.py
@@ -125,5 +125,102 @@ def create_3d_array():
     print(a)
 create_3d_array()
 
+# =================================================================
+# E.1:
+# Here is a search algorithm for arrays. The inputs are the array, which we call array; the number
+# n of elements in array; and target, the number being searched for. The output is the index in
+# array of target:
+# 1. Let min = 0 and max = n-1.
+# 2. Compute guess as the average of max and min, rounded down (so that it is an integer).
+# 3. If array[guess] equals target, then stop. You found it! Return guess.
+# 4. If the guess was too low, that is, array[guess] ¡ target, then set min = guess + 1.
+# 5. Otherwise, the guess was too high. Set max = guess - 1.
+# 6. Go back to step 2.
+# i. Convert the following algorithm to a proper code and explain the process of the search. ii.
+# What are the pro and cons of this method?
+# ----------------------------------------------------------------
 
 
+# =================================================================
+# E.2:
+# Work on a script to count the number of characters or (frequency) in a string.
+# ----------------------------------------------------------------
+string = "'Hello, my name is Brad.'"
+def freq_of_string(x):
+    count = 0
+    for i in x:
+        count += 1
+    print('The frequency of ' + x + ' is: ' + str(count))
+
+freq_of_string(string)
+
+# =================================================================
+# E.3:
+# Write a function that takes a list of words and returns the length of the longest one.
+# ----------------------------------------------------------------
+long_list = ['hello', 'goodbye', 'brad', 'cat']
+
+
+def longest_word(x):
+    lengths = []
+    for i in x:
+        count = 0
+        for j in i:
+            count +=1
+        lengths += [count]
+        max = lengths[0]
+        for k in lengths:
+            if k > max:
+                max = k
+    print(max)
+
+longest_word(long_list)
+
+# =================================================================
+# E.4:
+# Make up your own list and work on a program to get the smallest number from the list
+# ----------------------------------------------------------------
+long_list = ['hello', 'goodbye', 'brad', 'cat']
+def shortest_word(x):
+    lengths = []
+    for i in x:
+        count = 0
+        for j in i:
+            count +=1
+        lengths += [count]
+        min = lengths[0]
+        for k in lengths:
+            if k < min:
+                min = k
+    print(min)
+
+shortest_word(long_list)
+
+# =================================================================
+# E.5:
+# Work on a function that takes two lists and returns same (or True) if they have at least one
+# common element.
+# ----------------------------------------------------------------
+even = [2,4,6,8,]
+odd = [1,3,5,7,]
+mix = [1,2,3,4]
+
+def common_element(x,y):
+    for i in x:
+        for j in y:
+            if j == i:
+                print('True')
+                return
+common_element(even,odd)
+common_element(even,mix)
+
+# =================================================================
+# E.6:
+# Work on a script to merge or join two dictionaries.
+# ----------------------------------------------------------------
+
+
+# =================================================================
+# E.7:
+# Work on a script or a program to map two lists into a dictionary.
+# ----------------------------------------------------------------
