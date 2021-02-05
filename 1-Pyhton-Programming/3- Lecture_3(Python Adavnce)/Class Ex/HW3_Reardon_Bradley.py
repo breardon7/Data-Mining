c5116ec1503b9ac2abb10f4026e034d9e3a60b02
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-02-05 00:31:46 -0500

    Update HW3_Reardon_Bradley.py

---
+++ 1-Pyhton-Programming/3- Lecture_3(Python Adavnce)/Class Ex/HW3_Reardon_Bradley.py
@@ -0,0 +1,140 @@
+# =================================================================
+# Class_Ex1:
+# Writes a python script (use class) to simulate a Stopwatch .
+# push a button to start the clock (call the start method), push a button
+# to stop the clock (call the stop method), and then read the elapsed time
+# (use the result of the elapsed method).
+# ----------------------------------------------------------------
+class stopwatch:
+    def __init__(self, start, stop, reset)
+    self.start = start
+    self.stop = stop
+    self.reset = reset
+
+
+def start(stopwatch):
+    from time import clock
+    start_time = clock(stopwatch)
+
+def stop(stopwatch):
+    from time import clock
+    stop_time = clock(stopwatch)
+
+class counting_stopwatch:
+    def __init__
+
+
+# use sleep to simulate the program to check it is working correctly
+
+
+
+# =================================================================
+# Class_Ex2:
+# Write a python script (use class)to implement pow(x, n).
+# ----------------------------------------------------------------
+
+class pow:
+    def __init__(self, x, n):
+        self.x = x
+        self.n = n
+
+    def pow(self):
+        return x**n
+
+n = self(5, 10)
+
+
+
+
+
+
+
+
+# =================================================================
+# Class_Ex3:
+# Write a python class to calculate the area of rectangle by length
+# and width and a method which will compute the area of a rectangle.
+# ----------------------------------------------------------------
+
+class area_of_rect:
+    def __init__(self, length, width):
+        self.l = length
+        self.w = width
+
+    def calc(self):
+        return self.l * self.w
+
+area = area_of_rect(5, 10)
+area.calc()
+
+
+
+
+
+# =================================================================
+# Class_Ex4:
+# Write a python class and name it Circle to calculate the area of circle
+# by a radius and two methods which will compute the area and the perimeter
+# of a circle.
+# ----------------------------------------------------------------
+
+class Circle:
+    def __innit__(self, radius):
+        self.r = radius
+        from math import pi
+        def area:
+            return self.r**2 * pi
+        def perimeter:
+            return 2 * pi * self.r
+
+circle_radius = Circle(3)
+circle_radius.area()
+circle_radius.perimeter()
+
+
+
+
+# =================================================================
+# E.1
+# Write a script to find duplicates from an array (define an array with some duplicates on it). If
+# you use built in function in python explain the methods and how this methods are working.
+# ----------------------------------------------------------------
+
+
+
+
+
+# =================================================================
+# E.2:
+# Write a script that finds all such numbers which are divisible by 2 and 5, less than 1000. If you
+# use built in function in python explain the methods and how this methods are working.
+# ----------------------------------------------------------------
+
+
+
+
+
+
+# =================================================================
+# E.3:
+# Write a Python class to convert a roman numeral to an integer. Hint: (Use the following symbols
+# and numerals Wiki )
+# ----------------------------------------------------------------
+
+
+
+
+
+
+# =================================================================
+# E.4:
+# Write a Python class to find sum the three elements of the given array to zero.
+# Given: [-20, -10, -6, -4, 3, 4, 7, 10]
+# Output : [[-10, 3, 7], [-6, -4, 10]]
+# ----------------------------------------------------------------
+
+
+
+
+
+
