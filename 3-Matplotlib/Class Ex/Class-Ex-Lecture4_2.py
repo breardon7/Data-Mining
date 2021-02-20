c156a41718ea6f00b6494b540e99b818a454efe8
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-02-20 00:12:23 -0500

    Update Class-Ex-Lecture4_2.py

--- 3-Matplotlib/Class Ex/Class-Ex-Lecture4_2.py
+++ 3-Matplotlib/Class Ex/Class-Ex-Lecture4_2.py
@@ -6,9 +6,13 @@
 # Then plot it with the original curve np.exp(-X ** 2) in the range
 # (-3, 3) with 100 points in the range
 # ----------------------------------------------------------------
+import matplotlib.pyplot as plt
+import numpy as np
 
-
-
+x1 = np.linspace(-3,3,100)
+y1 = np.exp(-x1 ** 2)
+plt.plot(x1, y1)
+plt.show()
 
 
 # =================================================================
@@ -18,10 +22,20 @@
 # contains the y coordinates of the first curve, the third
 # column contains the y coordinates of the second curve, and so on.
 #  We want to display those N–1 curves.
-
 # ----------------------------------------------------------------
 
+# rand100 = np.random.randint(1,100,250)
+# reshaped100 = rand100.reshape(25, 10)
+# print(reshaped100)
 
+#x2 = np.linspace(0,6,100)
+#xreshape = x2.reshape(100,1)
+#y2 = np.exp(-x2**2).reshape(100,1)
+#y3 = np.exp(x2**3).reshape(100,1)
+#xplus = np.concatenate(xreshape, y2, y3)
+#plt.plot(x2,y2)
+#plt.plot(x2,y3)
+#plt.show()
 
 
 
@@ -33,7 +47,15 @@
 # ----------------------------------------------------------------
 data = np.random.rand(5,3)
 color_list = ['b', 'g', 'r', 'k', 'y']
+x = np.arange(3)
+bottom = np.cumsum(data, axis=0)
 
+for i in x:
+    if i == 0:
+        plt.bar(x, data[i], color = color_list[i])
+    else:
+        plt.bar(x, data[i], color = color_list[i], bottom = bottom[i-1])
+plt.show()
 
 
 
@@ -43,7 +65,13 @@ color_list = ['b', 'g', 'r', 'k', 'y']
 # on same plot with suitable legends of each line.
 # ----------------------------------------------------------------
 
-
+x4 = np.linspace(0,10,20)
+squared = x4**2
+cubed = x4**3
+plt.plot(x4, squared, label = 'x-squared')
+plt.plot(x4, cubed, label = 'x-cubed')
+plt.legend()
+plt.show()
 
 
 
